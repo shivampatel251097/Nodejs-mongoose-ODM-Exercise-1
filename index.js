b65c4b87a8e5a867709ef8847b5978db36e6258a
Shivam Patel <shivampatel251097@gmail.com>
2021-01-04 00:57:58 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/confusion';
+const connect = mongoose.connect(url);
+
+connect.then((db)=>{
+    console.log('Connected correctly to server');
+
+    var newDish = Dishes({
+        name: 'Shivam Patel',
+        description: 'First Test using mongoose'
+    });
+
+    newDish.save()
+    .then((dish)=>{
+        console.log(dish);
+
+        return Dishes.find({}).exec();
+    })
+    .then((dishes)=>{
+        console.log(dishes);
+
+        return Dishes.deleteMany({});
+    })
+    .then(()=>{
+        return mongoose.connection.close();
+    })
+});
